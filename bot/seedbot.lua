283b8c6ee9a09ebff1f95b59ebe12b1e21a52f8b
drivering <gitingbot@Gmail.com>
2016-06-25 07:02:17 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {161129693,182522528,211038089,207082609,224243644,165698005,136858697,235162148,238281750,200089490,188600928,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
